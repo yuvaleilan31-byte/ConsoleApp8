174f0e6c19dd4adacd3c9118c43914981a2c2c8e
Cyber_Mamriot <Cyber_Mamriot@CYBER-MAMRIOT>
2025-11-04 14:42:48 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        public static void RunMission1()
+        {
+            Console.WriteLine("RunMission1() is not implemented yet.");
+
+        }
+        public static void RunMission2()
+        {
+            Dictionary<string, int> highScores = new Dictionary<string, int>();
+            highScores.Add("NinjaMan", 9500);
+            highScores.Add("CodeQueen", 12000);
+            highScores.Add("PixelPal", 7200);
+            highScores["NinjaMan"] = 9900;
+            if (highScores.ContainsKey("Shadow"))
+                Console.WriteLine("Grade: " + highScores["Shadow"]);
+            else
+                Console.WriteLine("Player 'Shadow' has no score yet.");
+            foreach (var pair in highScores)
+            {
+                Console.WriteLine($" {pair.Key} : {pair.Value}");
+            }
+
+            highScores.Remove("PixelPal");
+
+        }
+
+        public static void RunMission3()
+        {
+            List<string> comments = new List<string>();
+
+            Console.WriteLine("RunMission3() is not implemented yet.");
+        }
+        public static void RunChallengeMission()
+        {
+            List<string> comments = new List<string>() { "What a great article!", "Buy Bitcoin now!!!", "This really helped me, thanks", "Amazing offer just for you", "How do I sign up?" };
+            int i = 0;
+            while (true)
+            {
+                if (comments[i].Contains("offer") || comments[i].Contains("Bitcoin"))
+                    comments.RemoveAt(i);
+                else
+                    i++;
+                if (i >= comments.Count)
+                    break;
+
+            }
+            Console.WriteLine("Filtered Comments:");
+            foreach (var comment in comments)
+            {
+                Console.WriteLine(comment);
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Mission 1: SocialApp (User Class Test) ---");
+            RunMission1();
+
+            Console.WriteLine("\n--- Mission 2: High Scores ---");
+            RunMission2();
+
+            Console.WriteLine("\n--- Mission 3: Console Piano ---");
+            RunMission3();
+
+            Console.WriteLine("\n--- Challenge Mission: Spam Filter ---");
+            RunChallengeMission();
+        }
+    }
+}
---
+++ ConsoleApp8/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ConsoleApp8")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a62216f0-d2fd-4992-bf13-73ba56d2ea43")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
